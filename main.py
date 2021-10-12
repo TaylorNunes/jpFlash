1764584716222dcc3e4ef8b263d1c39b776f3ebd
Taylor Nunes <taylorcnunes@gmail.com>
2021-10-12 16:30:55 -0400

    Did add different JLPT levels to scrape

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ from db_functions import make_db, check_table, make_table, insert_row, get_value
 conn = make_db()
 cur = conn.cursor()
 table_name = "vocab"
+source_urls = {
+  "n1":"https://jlptsensei.com/jlpt-n1-grammar-list/",
+  "n2":"https://jlptsensei.com/jlpt-n2-grammar-list/",
+  "n3":"https://jlptsensei.com/jlpt-n3-grammar-list/",
+  "n4":"https://jlptsensei.com/jlpt-n4-grammar-list/",
+  "n5":"https://jlptsensei.com/jlpt-n5-grammar-list/"
+}
+
 
 while True:
   command = input("\nEnter a command ('h' for options): ")
@@ -33,23 +41,42 @@ while True:
 
   # Gets the html for the list of grammar points and saves to db
   elif command == "s":
-    main_url = 'https://jlptsensei.com/jlpt-n3-grammar-list/page/5/'
-    main_response = requests.get(main_url)
-    main_soup = BeautifulSoup(main_response.text, 'html.parser')
-    grammar_row = main_soup.find_all('tr','jl-row')
-    print(len(grammar_row))
-    for irow in range(len(grammar_row)):
-        ex_num = 1
-        link = grammar_row[irow].find_all('a','jl-link')
-        sub_url = link[0]['href']
-        sub_response = requests.get(sub_url)
-        sub_soup = BeautifulSoup(sub_response.text, 'html.parser') 
+    user_level = input("Enter a level (n1-n5):")
+    first_url = source_urls[user_level]
+    
+    # Get all additional pages 
+    fu_response = requests.get(first_url)
+    fu_soup = BeautifulSoup(fu_response.text, 'html.parser')
+    soup_url_list_both = fu_soup.find_all("a", class_=lambda x: x == "page-numbers")
+    soup_url_list = []
+    for tag in soup_url_list_both:
+      if 'next' not in tag.attrs['class']:
+         soup_url_list.append(tag)
+    
+    url_list = []
+    url_list.append(first_url)
+    for iurl in range(len(soup_url_list)):
+      url_list.append(soup_url_list[iurl]['href'])
+    
+    page_num = 0
+    for main_url in url_list:
+      page_num += 1 
+      main_response = requests.get(main_url)
+      main_soup = BeautifulSoup(main_response.text, 'html.parser')
+      grammar_row = main_soup.find_all('tr','jl-row')
+      print("There are {0} grammar points on page {1}".format(len(grammar_row),page_num) )
+      for irow in range(len(grammar_row)):
+          ex_num = 1
+          link = grammar_row[irow].find_all('a','jl-link')
+          sub_url = link[0]['href']
+          sub_response = requests.get(sub_url)
+          sub_soup = BeautifulSoup(sub_response.text, 'html.parser') 
 
-        grammar_point = sub_soup.find_all('span','d-block p-3 text-centeret-gram')[0].find_all('span')[0].get_text()
-        jap_sentence = sub_soup.find_all('p','m-0 jp')[ex_num].get_text()
-        eng_sentence = sub_soup.find_all('div','alert alert-primary')[ex_num].get_text()
+          grammar_point = sub_soup.find_all('span','d-block p-3 text-centeret-gram')[0].find_all('span')[0].get_text()
+          jap_sentence = sub_soup.find_all('p','m-0 jp')[ex_num].get_text()
+          eng_sentence = sub_soup.find_all('div','alert alert-primary')[ex_num].get_text()
 
-        insert_row(cur, table_name, ("jlptsensei_g3",jap_sentence,eng_sentence,grammar_point))
+          insert_row(cur, table_name, ("jlptsensei_g{0}".format(user_level),jap_sentence,eng_sentence,grammar_point))
     conn.commit()
 
   # Gets cards from db and puts them into anki via anki connect
